d85dcb2f6c43608f22a5a6318d60c3a62f49de28
Bartosz Malec <bartosz.malec@onet.pl>
2023-03-18 16:42:50 +0100

    Added card models.

---
+++ src/main/java/com/github/splendor_mobile_game/database/CardDatabase.java
@@ -0,0 +1,31 @@
+package com.github.splendor_mobile_game.database;
+
+import com.github.splendor_mobile_game.enums.CardTier;
+import com.github.splendor_mobile_game.enums.TokenType;
+import com.github.splendor_mobile_game.model.Card;
+
+import java.util.ArrayList;
+
+public class CardDatabase {
+
+
+
+    public static ArrayList<Card> getAllCards(CardTier cardTier) {
+        ArrayList<Card> array = new ArrayList<>();
+
+        if (cardTier == CardTier.LEVEL_1) {
+            array.add(new Card(CardTier.LEVEL_1, 0, 2, 2, 0, 0, 0));
+            array.add(new Card(CardTier.LEVEL_1, 2, 1, 0, 1, 1, 0).setAdditionalToken(TokenType.SAPPHIRE));
+            array.add(new Card(CardTier.LEVEL_1, 1, 0, 0, 3, 3, 3));
+            array.add(new Card(CardTier.LEVEL_1, 0, 0, 0, 2, 1, 2).setAdditionalToken(TokenType.RUBY));
+            array.add(new Card(CardTier.LEVEL_1, 2, 0, 0, 2, 1, 2).setAdditionalToken(TokenType.DIAMOND));
+            array.add(new Card(CardTier.LEVEL_1, 1, 1, 2, 0, 1, 0));
+            array.add(new Card(CardTier.LEVEL_1, 1, 2, 3, 3, 0, 0));
+        }
+
+        return array;
+    }
+
+
+
+}
---
+++ src/main/java/com/github/splendor_mobile_game/model/Card.java
@@ -0,0 +1,67 @@
+package com.github.splendor_mobile_game.model;
+
+import com.github.splendor_mobile_game.enums.CardTier;
+import com.github.splendor_mobile_game.enums.TokenType;
+
+public class Card {
+
+    private final CardTier cardTier;
+
+    private TokenType additionalToken = null;
+
+    private final int points;
+
+    private final int emeraldCost;
+    private final int sapphireCost;
+    private final int rubyCost;
+    private final int diamondCost;
+    private final int onyxCost;
+
+
+    public Card(CardTier cardTier, int points, int emeraldCost, int sapphireCost, int rubyCost, int diamondCost, int onyxCost) {
+        this.cardTier     = cardTier;
+        this.points       = points;
+        this.emeraldCost  = emeraldCost;
+        this.sapphireCost = sapphireCost;
+        this.rubyCost     = rubyCost;
+        this.diamondCost  = diamondCost;
+        this.onyxCost     = onyxCost;
+    }
+
+    public CardTier getCardTier() {
+        return cardTier;
+    }
+
+    public TokenType getAdditionalToken() {
+        return additionalToken;
+    }
+
+    public Card setAdditionalToken(TokenType tokenType) {
+        this.additionalToken = tokenType;
+        return this;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public int getEmeraldCost() {
+        return emeraldCost;
+    }
+
+    public int getSapphireCost() {
+        return sapphireCost;
+    }
+
+    public int getRubyCost() {
+        return rubyCost;
+    }
+
+    public int getDiamondCost() {
+        return diamondCost;
+    }
+
+    public int getOnyxCost() {
+        return onyxCost;
+    }
+}
