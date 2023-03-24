4b3eea774edde342fba6e4c4a9a5139b2bdeca71
Krzysztof Kwiatkowski <kestivvi@gmail.com>
2023-03-24 22:40:11 +0100

    Modify Optional annotation

--- src/main/java/com/github/splendor_mobile_game/websocket/utils/json/Optional.java
+++ src/main/java/com/github/splendor_mobile_game/websocket/utils/json/Optional.java
@@ -1,5 +1,11 @@
 package com.github.splendor_mobile_game.websocket.utils.json;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface Optional {
 
 }
