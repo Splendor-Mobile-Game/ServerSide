35fa1655f140abffebf212a93372b56752e2d8f7
Krzysztof Kwiatkowski <kestivvi@gmail.com>
2023-03-25 16:51:42 +0100

    Add template classes

---
+++ src/main/java/com/github/splendor_mobile_game/websocket/handlers/reactions/Kick.java
@@ -0,0 +1,19 @@
+package com.github.splendor_mobile_game.websocket.handlers.reactions;
+
+import com.github.splendor_mobile_game.database.Database;
+import com.github.splendor_mobile_game.websocket.communication.ReceivedMessage;
+import com.github.splendor_mobile_game.websocket.handlers.Messenger;
+import com.github.splendor_mobile_game.websocket.handlers.Reaction;
+
+public class Kick extends Reaction {
+
+    public Kick(int connectionHashCode, ReceivedMessage receivedMessage, Messenger messenger, Database database) {
+        super(connectionHashCode, receivedMessage, messenger, database);
+    }
+
+    @Override
+    public void react() {
+        // TODO Auto-generated method stub
+    }
+    
+}
---
+++ src/main/java/com/github/splendor_mobile_game/websocket/handlers/reactions/SendChatMessage.java
@@ -0,0 +1,19 @@
+package com.github.splendor_mobile_game.websocket.handlers.reactions;
+
+import com.github.splendor_mobile_game.database.Database;
+import com.github.splendor_mobile_game.websocket.communication.ReceivedMessage;
+import com.github.splendor_mobile_game.websocket.handlers.Messenger;
+import com.github.splendor_mobile_game.websocket.handlers.Reaction;
+
+public class SendChatMessage extends Reaction {
+
+    public SendChatMessage(int connectionHashCode, ReceivedMessage receivedMessage, Messenger messenger, Database database) {
+        super(connectionHashCode, receivedMessage, messenger, database);
+    }
+
+    @Override
+    public void react() {
+        // TODO Auto-generated method stub
+    }
+    
+}
