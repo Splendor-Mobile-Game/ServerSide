0c29a7b49adbadb8fddefb90348e170d3808475b
Krzysztof Kwiatkowski <kestivvi@gmail.com>
2023-03-30 20:06:04 +0200

    Add specification to GetTokens

--- src/main/java/com/github/splendor_mobile_game/websocket/handlers/reactions/GetTokens.java
+++ src/main/java/com/github/splendor_mobile_game/websocket/handlers/reactions/GetTokens.java
@@ -5,7 +5,95 @@ import com.github.splendor_mobile_game.websocket.communication.ReceivedMessage;
 import com.github.splendor_mobile_game.websocket.handlers.Messenger;
 import com.github.splendor_mobile_game.websocket.handlers.Reaction;
 
-
+/**
+ * Players send this request if now is their turn and they want to get tokens from the table.
+ * In reaction server sends to all players message of type `GET_TOKENS_ANNOUNCEMENT` announcing that this has happend.
+ * 
+ * Example of user request
+ * {
+ *      "messageContextId": "02442d1b-2095-4aaa-9db1-0dae99d88e03",
+ *      "type": "GET_TOKENS",
+ *      "data": {
+ *          "tokensChange": {
+ *              "red": 1,
+ *              "blue": 1,
+ *              "green": 1,
+ *              "white": 0,
+ *              "black": 0
+ *          }
+ *      }
+ * }
+ * or something like this
+ * {
+ *      "messageContextId": "02442d1b-2095-4aaa-9db1-0dae99d88e03",
+ *      "type": "GET_TOKENS",
+ *      "data": {
+ *          "tokensChange": {
+ *              "red": 0,
+ *              "blue": 2,
+ *              "green": 0,
+ *              "white": 0,
+ *              "black": 0
+ *          }
+ *      }
+ * }
+ * if the situation arises that player would get more than 10 tokens in total,
+ * then player have to give back some other tokens, so the following might be possible.
+ * Consider a player has 9 tokens. They get 3 more and give back some 2, so the balance is 10 at max.
+ * {
+ *      "messageContextId": "02442d1b-2095-4aaa-9db1-0dae99d88e03",
+ *      "type": "GET_TOKENS",
+ *      "data": {
+ *          "tokensChange": {
+ *              "red": 1,
+ *              "blue": 1,
+ *              "green": 1,
+ *              "white": -2,
+ *              "black": 0
+ *          }
+ *      }
+ * }
+ * 
+ * Example of server announcement
+ * {
+ *      "messageContextId": "02442d1b-2095-4aaa-9db1-0dae99d88e03",
+ *      "type": "GET_TOKENS_ANNOUNCEMENT",
+ *      "result": "OK",
+ *      "data": {
+ *          "player": {
+ *              "uuid": "6850e6c1-6f1d-48c6-a412-52b39225ded7",
+ *          },
+ *          "tokensChange": {
+ *              "red": 1,
+ *              "blue": 1,
+ *              "green": 1,
+ *              "white": -2,
+ *              "black": 0
+ *          }
+ *      }
+ * }
+ *
+ * Some points about implementation:
+ * - We know what player has sent this message because we have their WebSocket's connectionHashCode.
+ * - Of course, we should update state of the game on the server (subtract appropriate amount of tokens for the purchase, add prestige points and add the bonus point)
+ * - The way of representation of this data in json message is arbitrary, you as developer decides. Then android client will have to adjust. What's showed here is exemplary.
+ * 
+ * Also.. Consider user is sending dodgy request, because they wants to cheat. 
+ * They send message to get tokens when there is turn of the other player.
+ * they want to get only one token, but games allows only 2 the same ones or 3 different each (and exchange when above 10)
+ * Please consider all possible scenarios, because this will be tested by testers.
+ * 
+ * In such invalid request you should send message only to the requester. For example
+ * {
+ *      "messageContextId": "02442d1b-2095-4aaa-9db1-0dae99d88e03",
+ *      "type": "GET_TOKENS_RESPONSE",
+ *      "result": "FAILURE"
+ *      "data": {
+ *          "error": "You cannot get tokens when this is not your turn!"
+ *      }
+ * }
+ * 
+ */
 public class GetTokens extends Reaction {
 
     public GetTokens(int connectionHashCode, ReceivedMessage receivedMessage, Messenger messenger, Database database) {
