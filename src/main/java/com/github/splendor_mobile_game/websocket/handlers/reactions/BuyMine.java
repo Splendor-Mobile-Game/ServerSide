6c1d61753dec00cec1789f42853a2443e884e183
Krzysztof Kwiatkowski <kestivvi@gmail.com>
2023-03-30 19:42:39 +0200

    Add specification to BuyMine

--- src/main/java/com/github/splendor_mobile_game/websocket/handlers/reactions/BuyMine.java
+++ src/main/java/com/github/splendor_mobile_game/websocket/handlers/reactions/BuyMine.java
@@ -4,8 +4,60 @@ import com.github.splendor_mobile_game.database.Database;
 import com.github.splendor_mobile_game.websocket.communication.ReceivedMessage;
 import com.github.splendor_mobile_game.websocket.handlers.Messenger;
 import com.github.splendor_mobile_game.websocket.handlers.Reaction;
+import com.github.splendor_mobile_game.websocket.handlers.ReactionName;
 
-
+/**
+ * Players sends this request if now is their turn and they want to buy mine (card) that is on the table.
+ * In reaction server sends to all players message of type `BUY_MINE_ANNONUCEMENT` announcing that this has happend.
+ * 
+ * Example of user request
+ * {
+ *      "messageContextId": "02442d1b-2095-4aaa-9db1-0dae99d88e03",
+ *      "type": "BUY_MINE",
+ *      "data": {
+ *          "card": {
+ *              "uuid": "521ba578-f989-4488-b3ee-91b043abbc83"
+ *          }
+ *      }
+ * }
+ * 
+ * Example of server announcement
+ * {
+ *      "messageContextId": "02442d1b-2095-4aaa-9db1-0dae99d88e03",
+ *      "type": "BUY_MINE_ANNONUCEMENT",
+ *      "result": "OK",
+ *      "data": {
+ *          "player": {
+ *              "uuid": "6850e6c1-6f1d-48c6-a412-52b39225ded7",
+ *          },
+ *          "card": {
+ *              "uuid": "521ba578-f989-4488-b3ee-91b043abbc83"
+ *          }
+ *      }
+ * }
+ *
+ * Some points about implementation:
+ * - We know what player has sent this message because we have their WebSocket's connectionHashCode.
+ * - uuid of card is the same that should have be send at the start of the game
+ * - Of course, we should update state of the game on the server (subtract appropriate amount of tokens for the purchase, add prestige points and add the bonus point)
+ * 
+ * Also.. Consider user is sending dodgy request, because they wants to cheat. 
+ * They send message to buy mine while is a turn of the other player.
+ * Or they send message to buy, but they don't have enough amount of tokens.
+ * Please think of every situation, because this will be tested by testers.
+ * 
+ * In such invalid request you should send message only to the requester. For example
+ * {
+ *      "messageContextId": "02442d1b-2095-4aaa-9db1-0dae99d88e03",
+ *      "type": "BUY_MINE_RESPONSE",
+ *      "result": "FAILURE"
+ *      "data": {
+ *          "error": "You cannot buy mine when this is not you turn!"
+ *      }
+ * }
+ * 
+ */
+@ReactionName("BUY_MINE")
 public class BuyMine extends Reaction {
 
     public BuyMine(int connectionHashCode, ReceivedMessage receivedMessage, Messenger messenger, Database database) {
