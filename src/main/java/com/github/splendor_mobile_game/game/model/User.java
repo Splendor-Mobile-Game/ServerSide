00b459bada61846e7a2463d59d2c995e9b0c5a2b
Dawid Walczak <dawidwalczak1404@gmail.com>
2023-04-06 23:19:15 +0200

    add purchased and reserved cards lists, add points variable

--- src/main/java/com/github/splendor_mobile_game/game/model/User.java
+++ src/main/java/com/github/splendor_mobile_game/game/model/User.java
@@ -1,5 +1,6 @@
 package com.github.splendor_mobile_game.game.model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -15,8 +16,14 @@ public class User {
 
     private int connectionHasCode;
 
+    private int points;
+
     //initialized tokens hashmap
     private Map<TokenType, Integer> tokens = new HashMap<TokenType, Integer>();
+
+    //initialized purchased and reserved cards lists
+    private ArrayList<Card> purchasedCards = new ArrayList<Card>();
+    private ArrayList<Card> reservedCards = new ArrayList<Card>();
 
     public User(UUID uuid, String name, int connectionHasCode) {
         this.uuid = uuid;
@@ -40,17 +47,27 @@ public class User {
         return result;
     }
 
-    //method which is adding two tokens
+    //method for adding two tokens (still needs to be validated if there are at least 4 tokens of this type on the table)
     public void takeTwoTokens(TokenType type) throws Exception {
         if(this.getTokenCount() > 8) throw new Exception("You can't have more than 10 tokens");
         this.tokens.put(type, this.tokens.get(type) + 2);
     }
 
-    //method which is taking tokens from user
+    //method for taking tokens from user
     public void putDownTokens(TokenType type, int amount) throws Exception {
         if(this.tokens.get(type) - amount < 0) throw new Exception("You can't have less than 0 tokens");
         this.tokens.put(type, this.tokens.get(type) - amount);
     }
+
+    //method for adding three different tokens
+    public void takeThreeTokens(TokenType type1, TokenType type2, TokenType type3) throws Exception {
+        if(type1 == type2 || type1 == type3 || type2 == type3) throw new Exception("No three different types selected");
+        this.tokens.put(type1, this.tokens.get(type1) + 1);
+        this.tokens.put(type2, this.tokens.get(type2) + 1);
+        this.tokens.put(type3, this.tokens.get(type3) + 1);
+    }
+
+    
 
     public int getConnectionHasCode() {
         return connectionHasCode;
