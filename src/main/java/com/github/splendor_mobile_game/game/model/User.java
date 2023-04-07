20a5826b296d26594bfd56f09b7334919e0d8a27
Krzysztof Kwiatkowski <kestivvi@gmail.com>
2023-04-07 23:11:31 +0200

    Format code

--- src/main/java/com/github/splendor_mobile_game/game/model/User.java
+++ src/main/java/com/github/splendor_mobile_game/game/model/User.java
@@ -3,61 +3,62 @@ package com.github.splendor_mobile_game.game.model;
 import java.util.Objects;
 import java.util.UUID;
 
-
-
 public class User {
 
-        private final String name;
-        private UUID uuid;
-        private int connectionHasCode;
-        private boolean hasPerformedAction;
-    
-        public User(UUID uuid, String name, int connectionHasCode) {
-            this.uuid = uuid;
-            this.name = name;
-            this.connectionHasCode = connectionHasCode;
-            this.hasPerformedAction = false;
-        }
-    
-        public int getConnectionHasCode() {
-            return connectionHasCode;
-        }
-    
-        public void setConnectionHasCode(int connectionHasCode) {
-            this.connectionHasCode = connectionHasCode;
-        }
-    
-        public String getName() {
-            return name;
-        }
-    
-        public UUID getUuid() {
-            return uuid;
-        }
-    
-        public void setUuid(UUID uuid) {
-            this.uuid = uuid;
-        }
-    
-        public boolean hasPerformedAction() {
-            return hasPerformedAction;
-        }
-    
-        public void setPerformedAction(boolean hasPerformedAction) {
-            this.hasPerformedAction = hasPerformedAction;
-        }
-    
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            User user = (User) o;
-            return name.equals(user.name) && uuid.equals(user.uuid) && connectionHasCode == user.connectionHasCode;
-        }
-    
-        @Override
-        public int hashCode() {
-            return Objects.hash(name, uuid, connectionHasCode);
-        }
-}
+    private final String name;
 
+    private UUID uuid;
+
+    private int connectionHasCode;
+
+    private boolean hasPerformedAction;
+
+    public User(UUID uuid, String name, int connectionHasCode) {
+        this.uuid = uuid;
+        this.name = name;
+        this.connectionHasCode = connectionHasCode;
+        this.hasPerformedAction = false;
+    }
+
+
+    public int getConnectionHasCode() {
+        return connectionHasCode;
+    }
+
+    public void setConnectionHasCode(int connectionHasCode) {
+        this.connectionHasCode = connectionHasCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public boolean hasPerformedAction() {
+        return hasPerformedAction;
+    }
+
+    public void setPerformedAction(boolean hasPerformedAction) {
+        this.hasPerformedAction = hasPerformedAction;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return name.equals(user.name) && uuid.equals(user.uuid) && connectionHasCode == user.getConnectionHasCode();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, uuid, connectionHasCode);
+    }
+}
