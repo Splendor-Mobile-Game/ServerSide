aa3027ad659c0341d0048e2fd311876c34bd0f9e
v4m3rr <igorurbanowicz@gmail.com>
2023-04-14 12:02:36 +0200

    Added declarations

--- src/main/java/com/github/splendor_mobile_game/websocket/handlers/ServerMessageType.java
+++ src/main/java/com/github/splendor_mobile_game/websocket/handlers/ServerMessageType.java
@@ -1,5 +1,7 @@
 package com.github.splendor_mobile_game.websocket.handlers;
 
+import com.github.splendor_mobile_game.websocket.handlers.reactions.BuyMine;
+
 // TODO: Java doc required
 public enum ServerMessageType {
 
@@ -7,6 +9,7 @@ public enum ServerMessageType {
     CREATE_ROOM_RESPONSE,
     JOIN_ROOM_RESPONSE,
     DEBUG_GET_RANDOM_CARD_RESPONSE,
+    BUY_MINE_RESPONSE,
     LEAVE_ROOM_RESPONSE,
     NEW_ROOM_OWNER
     
