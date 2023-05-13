6afcb4fcd54f7ff11074fde42b0b7ea824b497ba
Krystian Gumienik <krystian.gumienik@gmail.com>
2023-05-13 21:43:07 +0200

    added documentation and changed names of fields to be more clear

--- src/main/java/com/github/splendor_mobile_game/App.java
+++ src/main/java/com/github/splendor_mobile_game/App.java
@@ -13,16 +13,7 @@ import com.github.splendor_mobile_game.websocket.config.EnvConfig;
 import com.github.splendor_mobile_game.websocket.config.exceptions.InvalidConfigException;
 import com.github.splendor_mobile_game.websocket.handlers.ReactionManager;
 import com.github.splendor_mobile_game.websocket.handlers.connection.SimpleConnectionChecker;
-import com.github.splendor_mobile_game.websocket.handlers.reactions.BuyReservedMine;
-import com.github.splendor_mobile_game.websocket.handlers.reactions.BuyRevealedMine;
-import com.github.splendor_mobile_game.websocket.handlers.reactions.CreateRoom;
-import com.github.splendor_mobile_game.websocket.handlers.reactions.DebugGetRandomCard;
-import com.github.splendor_mobile_game.websocket.handlers.reactions.EndTurn;
-import com.github.splendor_mobile_game.websocket.handlers.reactions.GetTokens;
-import com.github.splendor_mobile_game.websocket.handlers.reactions.JoinRoom;
-import com.github.splendor_mobile_game.websocket.handlers.reactions.LeaveRoom;
-import com.github.splendor_mobile_game.websocket.handlers.reactions.MakeReservationFromDeck;
-import com.github.splendor_mobile_game.websocket.handlers.reactions.StartGame;
+import com.github.splendor_mobile_game.websocket.handlers.reactions.*;
 import com.github.splendor_mobile_game.websocket.utils.Log;
 
 /** This class represents the main application class for the Splendor game WebSocket server. */
@@ -30,7 +21,7 @@ public class App {
 
 	/** A list of classes that contain reactions to messages from clients. */
 	private static List<Class<?>> classesWithReactions = new ArrayList<>(Arrays.asList(
-		CreateRoom.class, JoinRoom.class, DebugGetRandomCard.class, LeaveRoom.class, GetTokens.class, StartGame.class, BuyRevealedMine.class, MakeReservationFromDeck.class, BuyReservedMine.class, EndTurn.class
+		CreateRoom.class, JoinRoom.class, DebugGetRandomCard.class, LeaveRoom.class, GetTokens.class, EndTurnTest.class, StartGame.class, BuyRevealedMine.class, MakeReservationFromDeck.class, BuyReservedMine.class, EndTurn.class
 	));
 
 
